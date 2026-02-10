4cd66bdcc2918212ab80dca574dae96f3f2d08d8
Bowen <bowenandbugs@gmail.com>
2026-02-10 14:50:37 -0800

    init

---
+++ src/main/java/edu/info5100/questapp/QuestAppApplication.java
@@ -0,0 +1,12 @@
+package edu.info5100.questapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestAppApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/info5100/questapp/QuestAppApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.info5100.questapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class QuestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
